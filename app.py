6ed07572fbd517706c2547699d44456e512ad36c
hp561 <hp561@ufl.edu>
2021-10-11 20:54:08 -0400

    updated logic

--- app.py
+++ app.py
@@ -16,25 +16,28 @@ def welcome():
 @app.route('/download', methods=["POST"])
 def download_video():
     link = request.get_json()
-    print("hello")
-    print(link.get('link'))
-    yt = YouTube(link.get('link'))
 
-    #Title of video
-    print("Title: "+yt.title)#Number of views of video
-    print("Number of views: ",yt.views)#Length of the video
-    print("Length of video: ",yt.length,"seconds")#Description of video
-    print("Description: ",yt.description)#Rating
-    print("Ratings: ",yt.rating)
+    try:
+        url = link.get('link', 'https://www.youtube.com/watch?v=23sp3cj5Pnc')
+        yt = YouTube(url)
 
-    print("Downloading...")
-    # audio_stream = yt.streams.filter(only_audio=True)[0].download(filename='audio.mp4')
-    ys = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(filename='video.mp4')
-    # video_stream = ffmpeg.input('video.mp4')
-    # audio_stream = ffmpeg.input('audio.mp4')
-    # ffmpeg.concat(video_stream, audio_stream, v=1, a=1).output('inished_video.mp4').run()
-    print("Download completed!!")
-    return send_from_directory('/Users/harsh/Documents/python', 'video.mp4', as_attachment=True)
+        #Title of video
+        print("Title: "+yt.title)#Number of views of video
+        print("Number of views: ",yt.views)#Length of the video
+        print("Length of video: ",yt.length,"seconds")#Description of video
+        print("Description: ",yt.description)#Rating
+        print("Ratings: ",yt.rating)
+
+        print("Downloading...")
+        # audio_stream = yt.streams.filter(only_audio=True)[0].download(filename='audio.mp4')
+        ys = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(filename='video.mp4')
+        # video_stream = ffmpeg.input('video.mp4')
+        # audio_stream = ffmpeg.input('audio.mp4')
+        # ffmpeg.concat(video_stream, audio_stream, v=1, a=1).output('inished_video.mp4').run()
+        print("Download completed!!")
+        return send_from_directory('/Users/harsh/Documents/python', 'video.mp4', as_attachment=True)
+    except Exception as e:
+        return str(e)
 
 
 
